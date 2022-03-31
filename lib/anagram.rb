efb7502a42e1367ed367c293d48ae021c4488586
Jacob Yaw <yawjacob@yahoo.com>
2022-03-30 20:46:01 -0500

    commit tree 95dfba3db66f031716f0c0313ad0ffb66fb82582

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+
+# Your class, Anagram, should take a word on 
+# initialization; instances should respond to 
+# a #match instance method that takes an array 
+# of possible anagrams. It should return all 
+# matches in an array. If no matches exist, 
+# it should return an empty array.
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        sorted_word_arr = @word.chars.sort
+        array.map do |arr_word| 
+            arr_word if arr_word.chars.sort == sorted_word_arr
+        end.compact
+    end
+end
